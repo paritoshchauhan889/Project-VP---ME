9d1ba5c4b7c3131ce9f35b262bd664eb2719d626
paritosh chauhan <paritoshstm889@gmail.com>
2024-03-01 11:22:12 +0530

    1-march-commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import {Routes , Route } from 'react-router-dom'
+import Home from './Home'
+import About from './Components/Pages/About'
 
 const App = () => {
   return (
-    <>
-      
-    </>
+    
+   <>
+    <Routes>
+      <Route  exact path="/" element={<Home/>}/>
+      <Route   path="/about" element={<About/>}/>
+    </Routes>
+   </>
   )
 }
 
